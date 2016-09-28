780fe16c6c69ba5682a6bbbb8eb375797379b71a
Sabhya Kaushal <sabhya001kaushal@gmail.com>
2016-09-29 02:18:22 +0530

    Create gender.py

---
+++ v_1/gender.py
@@ -0,0 +1,27 @@
+def gender(s):
+    while s[0:1] == ' ':
+        s = s[1:len(s)];
+    while s[len(s) - 1:len(s)] == ' ':
+        s = s[0:len(s) - 1];
+
+    name = s;
+    if len(s) < 3:
+        return "Invalid";
+
+    i = 26 + 25 + 24 + 22;
+    j = 26 + 25 + 24 + 23 + 7;
+
+    value = hex(ord(name[len(name) - 1].lower()));
+
+    if value == hex(j) or value == hex(i):
+        return "Female";
+
+    else:
+        return "Male";
+
+    return "Ambiguous";
+
+print(gender('Nandita'))
+print(gender('Priya'))
+print(gender('Raju'))
+print(gender('Sabhya'))
